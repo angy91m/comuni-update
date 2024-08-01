fbc78066fa33048677801ba1c8ece2fa1fb66639
Angelo Burzi <angelo.burzi@immobiliare.it>
2024-08-01 03:11:15 +0200

    updated

--- comuni-update.php
+++ comuni-update.php
@@ -7,9 +7,9 @@
         $d->setTime(0,0,0,0);
         return $d;
     }
-    
     $last_cycle = explode(',', file_get_contents(__DIR__ . '/bk/last-cycle.txt'));
     $comuni_old = json_decode(file_get_contents(__DIR__ . "/bk/$last_cycle[0]"), true);
+    $province = json_decode(file_get_contents('https://axqvoqvbfjpaamphztgd.functions.supabase.co/province'),true);
     $comuni_attivi = json_decode(file_get_contents('https://axqvoqvbfjpaamphztgd.functions.supabase.co/comuni'), true);
     $today_utc = date_create();
     $today_utc->setTime(0,0,0,0);
@@ -31,6 +31,22 @@
             $comuni_old[$k]['pendingDate'] = true;
         }
     }
+    foreach($comuni_attivi as $c) {
+        $found = false;
+        foreach($comuni_old as $co) {
+            if ($c['codiceCatastale'] == $co['codiceCatastale']) {
+                $found = true;
+                break;
+            }
+        }
+        if (!$found) {
+            $comuni_old[] = [
+                'nome' => mb_strtoupper( $c['nome'], 'UTF-8' ),
+                'provincia' => array_values(array_filter($province, function ($p) use ($c) {return $p['nome'] == $c['provincia']['nome'];}))[0],
+                'soppresso' => false
+            ] + $c;
+        }
+    }
     $last_cycle_d_arr = array_map(function($v){return intval($v);}, explode('-',$last_cycle[1]));
     $last_cycle_d = date_create();
     $last_cycle_d->setDate(...$last_cycle_d_arr);
@@ -44,17 +60,28 @@
         function($c) use ($last_cycle_d) {return get_italian_date($c['DATA_INIZIO_AMMINISTRATIVA'])->getTimestamp() >= $last_cycle_d->getTimestamp(); }
     );
     foreach($comuni_old as $k => $c) {
-        if (isset($c['soppresso']) && $c['soppresso'] && isset($c['pendingDate']) && $c['pendingDate']) {
-            $filtered = array_filter($soppressi, function($cs) {return $cs['PRO_COM_T'] == $c['codice'];});
-            if (count($filtered) > 0) {
-                unset($comuni_old[$k]['pendingDate']);
-                $d = get_italian_date($filtered[0]['DATA_INIZIO_AMMINISTRATIVA']);
-                $d->setTimezone(new DateTimeZone("UTC"));
-                $comuni_old[$k]['dataSoppressione'] = $d->format('Y-m-d\TH:i:s.v\Z');
-                $comuni_old[$k]['verso'] = $filtered[0]['PRO_COM_T_REL'];
-            }
+        if (!isset($c['codice'])) {
+            var_dump($c);
+            exit;
+        }
+        $filtered = array_values( array_filter($soppressi, function($cs) use ($c) {return $cs['PRO_COM_T'] == $c['codice'];}) );
+        if (count($filtered) > 0) {
+            unset($comuni_old[$k]['pendingDate']);
+            $d = get_italian_date($filtered[0]['DATA_INIZIO_AMMINISTRATIVA']);
+            $d->setTimezone(new DateTimeZone("UTC"));
+            $comuni_old[$k]['dataSoppressione'] = $d->format('Y-m-d\TH:i:s.v\Z');
+            $comuni_old[$k]['soppresso'] = true;
+            $comuni_old[$k]['verso'] = $filtered[0]['PRO_COM_T_REL'];
         }
     }
+    $chars = 'ABCDEFGHILMNOPQRSTUVZ';
+    usort($comuni_old, function($a, $b) use ($chars) {
+        $res = strpos($chars, $a['codiceCatastale'][0]) - strpos($chars, $b['codiceCatastale'][0]);
+        if ($res == 0) {
+            $res = intval(substr($a['codiceCatastale'],1,3)) - intval(substr($b['codiceCatastale'],1,3));
+        }
+        return $res;
+    });
     $last_cycle_s = $last_cycle_d->format('Y-m-d');
     if (count($soppressi) > 0) {
         $md = max(array_map(function($c) {return get_italian_date($c['DATA_INIZIO_AMMINISTRATIVA'])->getTimestamp();}, $soppressi));
@@ -63,5 +90,5 @@
         $last_cycle_s = $d->format('Y-m-d');
     }
     $filename = 'comuni-all-' . date_create()->format('YmdHis') . '.json';
-    file_put_contents(__DIR__ . "/bk/$filename", json_encode($comuni_old, JSON_PRETTY_PRINT));
+    file_put_contents(__DIR__ . "/bk/$filename", json_encode($comuni_old, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
     file_put_contents(__DIR__ . '/bk/last-cycle.txt', $filename . ',' . $last_cycle_s);
